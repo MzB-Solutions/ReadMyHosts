bd5454812988b70bc8f852411027e7d83b780fe2
Mitos Kalandiel <mitoskalandiel@gmail.com>
2021-06-18 21:12:15 +0100

    we've included the "legacy" code from windows only

---
+++ ReadMyHosts.Core/Models/HostModel.cs
@@ -0,0 +1,17 @@
+using PostSharp.Patterns.Contracts;
+
+namespace ReadMyHosts.Core.Models
+
+{
+    public class Host
+    {
+        [Required]
+        public byte[] FullIp { get; set; } = new byte[4];
+
+        [Required]
+        public int HostId { get; set; }
+
+        [Required]
+        public string HostName { get; set; }
+    }
+}
