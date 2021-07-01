fb8325af5b2d58703954ef6439d61df074d356fa
Sebastian Meier zu Biesen <smeierzubiesen@gmail.com>
2021-07-01 19:26:13 +0100

    Changed OS detection logic into a 4 binary field to switch case through and throw exception when needed

--- ReadMyHosts.Core/Info.cs
+++ ReadMyHosts.Core/Info.cs
@@ -10,12 +10,15 @@ namespace ReadMyHosts.Core
 
         public Info()
         {
+            SetOS();
             SetHostsRootPath();
         }
 
         #endregion Public Constructors
 
         #region Public Properties
+
+        public string CustomPath { get; set; }
 
         [Required]
         public string DirectorySeparator { get; set; }
@@ -25,33 +28,86 @@ namespace ReadMyHosts.Core
 
         #endregion Public Properties
 
-        #region Private Properties
+        #region Private Fields
 
-        private static bool IsLinux { get; } = RuntimeInformation.IsOSPlatform(OSPlatform.Linux);
+        private OSType SystemType;
 
-        private static bool IsWindows { get; } = RuntimeInformation.IsOSPlatform(OSPlatform.Windows);
+        #endregion Private Fields
 
-        #endregion Private Properties
+        #region Private Enums
+
+        [Flags]
+        private enum OSType
+        {
+            None = 0b_0000,
+            Windows = 0b_0001,
+            Linux = 0b_0010,
+            FreeBSD = 0b_0011,
+            OSX = 0b_00100
+        }
+
+        #endregion Private Enums
 
         #region Private Methods
 
         private void SetHostsRootPath()
         {
-            if (IsLinux)
+            switch (SystemType)
             {
-                DirectorySeparator = "/";
-                RootPath = "/";
+                case OSType.None:
+                    // by virtue of using Required from PostSharp we get an exception if no valid OS found
+                    RootPath = string.Empty;
+                    DirectorySeparator = string.Empty;
+                    break;
+
+                case OSType.Windows:
+                    DirectorySeparator = "\\";
+                    RootPath = String.Format("C:{0}Windows{0}System32{0}drivers{0}", DirectorySeparator);
+                    break;
+
+                case OSType.Linux:
+                    DirectorySeparator = "/";
+                    RootPath = "/";
+                    break;
+
+                case OSType.FreeBSD:
+                    DirectorySeparator = "/";
+                    RootPath = "/";
+                    break;
+
+                case OSType.OSX:
+                    throw new NotImplementedException();
+
+                default:
+                    throw new AccessViolationException();
             }
-            if (IsWindows)
+        }
+
+        private void SetOS()
+        {
+            bool IsBSD = RuntimeInformation.IsOSPlatform(OSPlatform.FreeBSD);
+            bool IsLinux = RuntimeInformation.IsOSPlatform(OSPlatform.Linux);
+            bool IsOSX = RuntimeInformation.IsOSPlatform(OSPlatform.OSX);
+            bool IsWindows = RuntimeInformation.IsOSPlatform(OSPlatform.Windows);
+            if (IsBSD)
             {
-                DirectorySeparator = "\\";
-                RootPath = String.Format("C:{0}Windows{0}System32{0}drivers{0}", DirectorySeparator);
+                SystemType = OSType.FreeBSD;
             }
-            if (!IsLinux && !IsWindows)
+            else if (IsLinux)
             {
-                // by virtue of using Required from PostSharp we get an exception if no valid OS found
-                RootPath = String.Empty;
-                DirectorySeparator = String.Empty;
+                SystemType = OSType.Linux;
+            }
+            else if (IsOSX)
+            {
+                SystemType = OSType.OSX;
+            }
+            else if (IsWindows)
+            {
+                SystemType = OSType.Windows;
+            }
+            else
+            {
+                SystemType = OSType.None;
             }
         }
 
